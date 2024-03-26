9564046d4cd530a7a1a69b023c2b545ea561952a
Rayen Akrich <akricherayen@gmail.com>
2024-03-26 13:54:33 +0100

    Create Types.h

---
+++ Types.h
@@ -0,0 +1,23 @@
+#ifndef NEURONE_TYPES_H
+#define NEURONE_TYPES_H
+
+//Type neurone
+typedef struct Neurone
+{
+    int num_xi; //Nombre d'entrées du neurone
+    //L'utilisation de  "double" plutôt que "int" ou "float" pour une meilleure précision des calculs
+    double *xi; //Tableau pour stocker les valeurs d'entrée
+    double *wi; //Tableau pour stocker les poids (weight) associés à chaque entrée
+    double bias; // Bias ajuste la sortie du neurone indépendamment des valeurs d'entrée, permettant ainsi de représenter des motifs même lorsque les valeurs d'entrée sont nulles
+    double yi; //Valeur de sortie après activation
+    int activation_function;//Type de fonction d'activation pour le neurone (il prendra 1,2,3)
+} Neurone;
+
+//Type couche
+typedef struct Couche
+{
+    int num_neurones; //Nombre de neurones dans la couche
+    Neurone **neurones; //Tableau de pointeurs de neurones
+}Couche;
+
+#endif
