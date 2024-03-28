2c50794f0bd6d2288215650372e60e38ba1aa4fc
Ghofrane Dhaouadi <164937936+ghofranedhaouadi02@users.noreply.github.com>
2024-03-28 15:09:40 +0100

    Types(liste chainée).h

---
+++ Types(liste chainée).h
@@ -0,0 +1,31 @@
+//Cette librairie contient les types caractérisant le réseau neurone
+
+#ifndef NEURONE_TYPES_H
+#define NEURONE_TYPES_H
+
+//Type neurone
+typedef struct Neurone
+{
+    int num_xi; //Nombre d'entrées du neurone
+    //L'utilisation de  "double" plutôt que "int" ou "float" pour une meilleure précision des calculs
+    double *xi; //Tableau pour stocker les valeurs d'entrée
+    double *wi; //Tableau pour stocker les poids (weight) associés à chaque entrée
+    double bias; //Bias ajuste la sortie du neurone indépendamment des valeurs d'entrée,
+    double yi; //Valeur de sortie après activation
+    int activation_function;//Type de fonction d'activation pour le neurone (il prendra 1,2,3)
+} Neurone;
+
+//Type couche
+typedef struct Couche
+{
+    int num_neurones; //Nombre de neurones dans la couche
+    Neurone **neurones; //Tableau de pointeurs de neurones
+    couche *next_couche; // Pointeur vers la couche suivante dans le réseau
+}Couche;
+
+//Type réseau neurone
+typedef struct Reseau{
+    Couche *first_couche; //Pointeur vers la première couche de réseau
+}Reseau;
+
+#endif
