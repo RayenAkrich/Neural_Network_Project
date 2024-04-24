801836ed1f838a39d3f229952cbefafcae3d86b4
Rayen Akrich <akricherayen@gmail.com>
2024-04-24 14:48:18 +0100

    Update main.c

--- Tentative_Test_1_Neurone/main.c
+++ Tentative_Test_1_Neurone/main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Manipulation.h"
+#include "Gradient_descent.h"
+#include "Outils_optimisation.h"
+#include "Derivative_activation_functions.h"
+#include "Fonctions_activation.h"
+#include "Save_load.h"
+#include "Types.h"
+#include "Train_single_neurone.h"
 
+int main() {
+    train_single_neuron_network();
+    return 0;
+}
