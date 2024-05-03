27d9a101daa376f0456d447e86cc202fee713f0c
Rayen Akrich <akricherayen@gmail.com>
2024-05-03 10:43:27 +0100

    Update Save_load.h

--- Tentative_Test_1_Neurone/Save_load.h
+++ Tentative_Test_1_Neurone/Save_load.h
@@ -1,9 +1,37 @@
 #include <stdio.h>
 #include "neurone_types.h"
 #include "Types.h"
+#include "stdlib.h"
 
 #ifndef NEURONE_SAVELOAD_H
 #define NEURONE_SAVELOAD_H
+
+#define MAX_ROWS 100
+#define MAX_COLS 3
+
+typedef struct {
+    double feature1;
+    double feature2;
+    double label;
+} DataRow;
+
+DataRow data[MAX_ROWS];
+
+int readDataFromFile(const char* filename){
+    FILE* file=fopen(filename,"r");
+    if (!file){
+        printf("Impossible d'ouvrir le fichier.\n");
+        return -1;
+    }
+
+    int row=0;
+    while(row < MAX_ROWS && fscanf(file,"%ld,%lf,%lf\n",&data[row].feature1,&data[row].feature2,&data[row].label)==3){
+        row++;
+    }
+
+    fclose(file);
+    return row;
+}
 
 // Sauvegarder les poids et les biais dans un fichier
 void sauvegarder_reseau(Reseau* reseau, const char* fichier) {
