3c82a0e6775f8e1e8787599b3b9039efcc278c37
Rayen Akrich <akricherayen@gmail.com>
2024-05-03 11:35:25 +0100

    Update Save_load.h

--- Tentative_Test_1_Neurone/Save_load.h
+++ Tentative_Test_1_Neurone/Save_load.h
@@ -60,6 +60,17 @@ void charger_reseau(Reseau* reseau, const char* fichier) {
         exit(EXIT_FAILURE);
     }
 
+    // Vérifier la taille du fichier
+    fseek(f, 0, SEEK_END); // Se déplacer à la fin du fichier
+    long taille_fichier = ftell(f); // Obtenir la taille du fichier
+    fseek(f, 0, SEEK_SET); // Se déplacer au début du fichier
+
+    if (taille_fichier == 0) {
+        printf("Le fichier %s est vide. Aucune donnée à charger.\n", fichier);
+        fclose(f);
+        return; // Quitter la fonction si le fichier est vide
+    }
+
     for (int i = 0; i < reseau->num_couches; ++i) {
         for (int j = 0; j < reseau->couches[i]->num_neurones; ++j) {
             Neurone* neurone = reseau->couches[i]->neurones[j];
