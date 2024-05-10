e7b7792886ac11a6d8c59aaa1bf1e2d36b110b9e
Rayen Akrich <akricherayen@gmail.com>
2024-05-10 15:40:27 +0100

    Update Gradient_descent.h

--- Tentative_Test_1_Neurone/Gradient_descent.h
+++ Tentative_Test_1_Neurone/Gradient_descent.h
@@ -5,7 +5,7 @@
 #ifndef NEURONE_GRADIENT_DESCENT_H
 #define NEURONE_GRADIENT_DESCENT_H
 
-double gradient_descent(double input, double target_output, double learning_rate, int num_iterations, double *weight, double *bias) {
+void gradient_descent(double input, double target_output, double learning_rate, int num_iterations, double *weight, double *bias) {
     double predicted_output, error,loss_value;
 
     for (int i = 0; i < num_iterations; i++) {
@@ -24,7 +24,6 @@ double gradient_descent(double input, double target_output, double learning_rate
         printf("Iteration %d: Predicted Output = %.4f, Error = %.4f, Loss = %.4f\n", i+1, predicted_output, error, loss_value);
     }
     printf("FINAL Predicted Output = %.4f, FINAL Error = %.4f, FINAL Loss = %.4f\n", predicted_output, error, loss_value);
-    return predicted_output;
 }
 
 #endif
